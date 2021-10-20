26f44b6c12d56ee6e6bef193e93e4a6c4bded605
Christopher Lu <chlu@ruby.cecs.pdx.edu>
2021-10-20 12:18:41 -0700

    first commit

---
+++ countsort.py
@@ -0,0 +1,10 @@
+#this is a python implementation of counting sort
+#this is a numeric counting sort that should return
+
+def countingsort(A):
+
+#find the starting index for each number?
+   
+
+   #produce an occurrance list that counts the volume of each number in the array
+    #for each unique set of values in the 
